61f26f07c3f5d080ad04c11ae8c5bf2087de15ac
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-05-29 23:29:14 +0700

    rev1

--- app/src/main/java/com/andorid/visitjogja/SignActivity.java
+++ app/src/main/java/com/andorid/visitjogja/SignActivity.java
@@ -1,24 +1,63 @@
 package com.andorid.visitjogja;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        String emailNew = emailEditText.getText().toString();
+        String userNew = usernameEditText.getText().toString();
+        String passNew = passwordEditText.getText().toString();
+
+        emailEditText = findViewById(R.id.inputNewEmail);
+        usernameEditText = findViewById(R.id.inputNewUsername);
+        passwordEditText = findViewById(R.id.inputNewPassword);
+        if (emailNew.isEmpty()){
+            emailEditText.setError("Email cannot be empty");
+        }
+        if (userNew.isEmpty()) {
+            usernameEditText.setError("Username cannot be empty");
+        }
+        if (passNew.isEmpty()){
+            passwordEditText.setError("Password cannot be empty");
+        }
+        else{
+            auth.createAccount(user, pass, email).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignActivity.this, LoginActivity.class));
+                    } else {
+                        Toast.makeText(SignActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
-}
+});
